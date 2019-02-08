earl/llvm-mirror

commit 72bc91ff968d1da4647fdd7737b6c851e917ab38

[WebAssembly] Fixed Disassembler ignoring endian swap on big endian.
Summary: This fixes: https://bugs.llvm.org/show_bug.cgi?id=40620

Reviewers: aheejin

Subscribers: dschuff, sbc100, jgravelle-google, sunfish, llvm-commits

Tags: #llvm

Differential Revision: https://reviews.llvm.org/D57933

git-svn-id: https://llvm.org/svn/llvm-project/llvm/trunk@353496 91177308-0d34-0410-b5e6-96231b3b80d8

lib/Target/WebAssembly/Disassembler/WebAssemblyDisassembler.cpp
@@ -108,9 +108,8 @@ template <typename T>
 bool parseImmediate(MCInst &MI, uint64_t &Size, ArrayRef<uint8_t> Bytes) {
   if (Size + sizeof(T) > Bytes.size())
     return false;
-  T Val;
-  memcpy(&Val, Bytes.data() + Size, sizeof(T));
-  support::endian::byte_swap<T, support::endianness::little>(Val);
+  T Val = support::endian::read<T, support::endianness::little, alignof(T)>(
+      Bytes.data() + Size);
   Size += sizeof(T);
   if (std::is_floating_point<T>::value) {
     MI.addOperand(MCOperand::createFPImm(static_cast<double>(Val)));

commit 0ae15aac8c9f7d0a7148abd40ac62ca72ce5ac1d

[WebAssembly] Fix parseImmediate's memory alignment requirement
This fixes the current failure in the x86-64 ubsan bot caused by
r353496.

git-svn-id: https://llvm.org/svn/llvm-project/llvm/trunk@353499 91177308-0d34-0410-b5e6-96231b3b80d8

lib/Target/WebAssembly/Disassembler/WebAssemblyDisassembler.cpp
@@ -108,7 +108,7 @@ template <typename T>
 bool parseImmediate(MCInst &MI, uint64_t &Size, ArrayRef<uint8_t> Bytes) {
   if (Size + sizeof(T) > Bytes.size())
     return false;
-  T Val = support::endian::read<T, support::endianness::little, alignof(T)>(
+  T Val = support::endian::read<T, support::endianness::little, 1>(
       Bytes.data() + Size);
   Size += sizeof(T);
   if (std::is_floating_point<T>::value) {